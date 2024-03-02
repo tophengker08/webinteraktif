5c5a68f737ad8a482c58cf8bbbb044fafead7318
tophengker08 <nombatimothy@gmail.com>
2024-03-02 13:20:32 +0900

    rev7

--- page2.py
+++ page2.py
@@ -1,5 +1,8 @@
 import streamlit as st
 def page_2():
     st.title("CARA MENJADI KAYA")
-    st.write('MAU JADI KAYA TONTO VIDEO INI!!')
+    st.write('MAU JADI KAYA? TONTO VIDEO INI!!')
     st.video('https://youtu.be/gs-F5seOoOk?si=jLF2_q9x_553EBzj')
+    st.title("CARA SUKSES DI UMUR 18")
+    st.write('MAU JADI SUKSES? TONTO VIDEO INI!!')
+    st.video('https://youtu.be/bTZzgXxRhFo?si=1ODgBNHiSmP1ty-9')
