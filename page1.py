e88e9e93687c0efe70c5838874f193633c54f190
tophengker08 <nombatimothy@gmail.com>
2024-03-02 12:55:13 +0900

    rev2

--- page1.py
+++ page1.py
@@ -1,4 +1,8 @@
 import streamlit as st
 def page_1():
-    st.title("Halaman 1")
+    st.title("PENGANTAR")
     st.write('Halaman ini digunakan untuk Intro')
+    st.write('87% Murid di Akademi berhasil melipatgandakan portofolionya dalam waktu 3 bulan menggunakan strategi kita. Ayo bergabung!!!')
+    st.write('https://akademicrypto.com/')
+    
+    
--- page2.py
+++ page2.py
@@ -1,5 +1,5 @@
 import streamlit as st
 def page_2():
-    st.title("Halaman 2")
+    st.title("CARA MENJADI KAYA")
     st.write('Halaman ini digunakan untuk Menampilkan youtube')
     st.video('https://youtu.be/gs-F5seOoOk?si=jLF2_q9x_553EBzj')
