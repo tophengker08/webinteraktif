39e1cc36bac6c5fc5ad2df0b30eace153a0716df
tophengker08 <nombatimothy@gmail.com>
2024-03-02 13:30:59 +0900

    rev9

--- page1.py
+++ page1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 def page_1():
     st.title("Strategi Trading Crypto Untuk Gandain Uang Kalian")
-    st.title('BY : AKADEMICRYPTO')
+    st.title('by: akademicrypto')
     st.write('87% Murid di Akademi berhasil melipatgandakan portofolionya dalam waktu 3 bulan menggunakan strategi kita.')
     st.write('kalian akan dibimbing dari 0 sampai bisa melipatgandakan portofolio melalui aset crypto dengan 200+ modul E-learning, yang bisa di akses 24 jam dan di update setiap bulannya yang diajar langsung dengan pakar Phd.Kalian akan mendapatkan akses ke Ekslusif Riset independen dari team Akademi Crypto, untuk mengetahui narative apa yang akan jalan duluan di cycle selanjutnya, testimoni bisa dibuktikan sendiri dengan bergabung di group discord gratis kita.')
     st.write('https://akademicrypto.com/')
