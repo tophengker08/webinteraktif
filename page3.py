562257d15cfdffc0e450ec5eaa8cb5389a01828c
tophengker08 <nombatimothy@gmail.com>
2024-03-02 13:16:04 +0900

    rev6

--- page3.py
+++ page3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 def page_3():
-    st.title("Halaman 3")
+    st.title("CONTOH")
     st.write('Menampilkan Halaman dari file MD (MarkDown)')
     
     with open('file page 3.md', 'r') as file:
