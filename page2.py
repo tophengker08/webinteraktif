1783e8c0c630499aa1ebfb8056298669dabe62b2
tophengker08 <nombatimothy@gmail.com>
2024-03-02 13:12:19 +0900

    rev5

--- page2.py
+++ page2.py
@@ -1,5 +1,5 @@
 import streamlit as st
 def page_2():
     st.title("CARA MENJADI KAYA")
-    st.write('Halaman ini digunakan untuk Menampilkan youtube')
+    st.write('MAU JADI KAYA TONTO VIDEO INI!!')
     st.video('https://youtu.be/gs-F5seOoOk?si=jLF2_q9x_553EBzj')
