137b914167f91c3365e89f23fa17ca33c20ef803
tophengker08 <nombatimothy@gmail.com>
2024-03-04 14:00:10 +0900

    rev5

--- lat1.py
+++ lat1.py
@@ -2,6 +2,7 @@ import streamlit as st
 from page1 import page_1
 from page2 import page_2
 from page3 import page_3
+from page4 import page_4
 
 # import pandas as pd
 
@@ -19,7 +20,8 @@ from page3 import page_3
 PAGES = {
     "Page 1" : page_1,
     "Page 2" : page_2,
-    "Page 3" : page_3
+    "Page 3" : page_3,
+    "Page 4" : page_4
     }
     
 st.sidebar.image("cryptologo.jpg", width=200)
---
+++ page4.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+def page_4():
+    st.title('Kalkulator Hitung Segitiga')
+    panjang = st.number_input("Masukan Nilai Panjang", 0)
+    lebar = st.number_input("masukan Nilai Lebar", 0)
+    hitung = st.button("Hitung Luas")
+    
+    if hitung:
+        luas = panjang * lebar /2
+        st.success(f"Luas Segitiga adalah={luas}")
+    
